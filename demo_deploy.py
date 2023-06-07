5b3463d383e010f2fb28baf9709a3cf4b5ddf6b8
Patrick Fu <49091150+PatrickFu0302@users.noreply.github.com>
2023-06-07 14:06:19 +0800

    Update demo_deploy.py

--- demo_deploy.py
+++ demo_deploy.py
@@ -13,8 +13,8 @@ path_data = 'https://github.com/PatrickFu0302/time-series-feature-demo/main/'
 
 @st.cache_data
 def read_data():
-    pca_matrix = []
-    tsne_matrix = []
+    pca_matrix = pd.read_csv(os.path.join(path_data, 'pca_matrix.csv'))
+    tsne_matrix = pd.read_csv(os.path.join(path_data, 'tsne_matrix.csv'))
     return pca_matrix, tsne_matrix
 
 data_PCA, data_tSNE_2d = read_data()
